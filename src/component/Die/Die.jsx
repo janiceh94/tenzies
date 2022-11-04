6fc58d9ab2621d1940a443a18207f6b0b17209e2
janiceh94 <haesung94@gmail.com>
2022-11-03 21:41:58 -0700

    function to generate 10 random numbers from 1 - 6

--- src/component/Die/Die.jsx
+++ src/component/Die/Die.jsx
@@ -2,6 +2,15 @@ import React from "react"
 import "./Die.css"
 
 export default function Die(props){
+
+    function allNewDice(){
+        const newDice = [];
+        for(let i = 0; i < 10; i ++){
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+
     return (
         <div className="die-face">
             <h2 className="die-number">{props.value}</h2>
