c3f906147161500317f96fd6b032c6fe842d27ad
janiceh94 <haesung94@gmail.com>
2022-11-07 15:57:05 -0800

    confetti appears when user completes the game

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import Die from "./component/Die/Die"
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 function App() {
 
@@ -68,6 +69,7 @@ function App() {
 
   return (
     <main>
+      {tenzies && <Confetti/>}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
