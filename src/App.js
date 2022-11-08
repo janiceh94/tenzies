ac85e625eec17bd26194d367293f0801c0e2b8a3
janiceh94 <haesung94@gmail.com>
2022-11-07 16:01:04 -0800

    can play a new game

--- src/App.js
+++ src/App.js
@@ -40,10 +40,15 @@ function App() {
   }
 
   function rollDice(){
-    setDice(oldDice => oldDice.map(die => {
-      return die.isHeld ? 
-        die : generateNewDie()
-    }))
+    if(!tenzies){
+      setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ? 
+          die : generateNewDie()
+      }))
+    } else {
+      setTenzies(false)
+      setDice(allNewDice())
+    }
   }
 
   function holdDice(id){
